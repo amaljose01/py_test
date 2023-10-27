47b7a0f9755b98df9de035b0370d6c917563c282
Amal Jose <amaljom@gmail.com>
2023-10-27 18:50:39 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,21 +1,9 @@
 import pandas as pd
 import tabulate
-#import os
 
-#df = pd.read_json('us10trial_jso.json')
 df = pd.read_json ('/Users/I347708/Desktop/OFB/us10trial_jso.json')
-
 df1= df.loc[df['type'].isin(['OFFBOARDING'])]
 df2= df1.sort_values(by='createdAt', ascending=False)
-#new_df = df2.loc[:, ['id','name','tenantId','status','maxRetry','executionCount','context']]
 new_df = df2.loc[:, ['id','name','tenantId','status','maxRetry','executionCount']]
-#new_df.to_csv('offboarding_pending.csv')
 
-#print(tabulate(new_df)
 print(new_df.to_markdown())
-
-#pvtlist = df.pivot_table(index=['context'], aggfunc='size')
-#print(new_df)
-#pvtlist.to_csv('context_csv.csv')
-
-#os.remove('offboarding_pending.csv'
